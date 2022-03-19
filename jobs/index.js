00f9b086584fb6d447a29c0fe7606a076ea1c18c
Eric <eric23b@icloud.com>
2022-03-19 08:49:16 -0600

    Added jobs page.

---
+++ jobs/index.js
@@ -0,0 +1,12 @@
+import {getDBEntrees, insertDBEntry} from "../db-utilities.js";
+import {
+    LOGS_SCHEMA,
+    TIMER_TABLE,
+    BUSINESS_SCHEMA,
+    EMPLOYEES_TABLE,
+    STATIONS_TABLE,
+    JOBS_TABLE
+} from "../directives.js";
+
+const serverSettings = {}
+
