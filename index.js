ceffe46c6522ce6a11906c056dcaaaa691bfde95
Eric <eric23b@icloud.com>
2022-07-17 16:58:42 -0600

    Added index.js.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import  {
+    showAllMessages,
+} from "./messaging.js";
+
+const theme = getLocalStorageValue('theme') || "light";
+document.documentElement.setAttribute('data-color-theme', theme);
+
+function getLocalStorageValue(key) {
+    if (window.localStorage[key])
+        return JSON.parse(window.localStorage.getItem(key));
+}
+
+const messagesIcon = document.querySelector('#messages');
+messagesIcon.onclick = showAllMessages;
