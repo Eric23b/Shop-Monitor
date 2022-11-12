a0b86b6be5b5cc908e3994966445247cadf6381a
Eric <eric23b@icloud.com>
2022-11-12 07:03:51 -0600

    Removed userName from getUserInfo().

--- db-utilities.js
+++ db-utilities.js
@@ -3,7 +3,6 @@ export async function getUserInfo(serverSettings) {
     const headers = getBasicHeaders(serverSettings.authorization);
     const raw = JSON.stringify({
         operation: "user_info",
-        userName: "IssuesAdmin",
     });
     const requestOptions = buildRequestOptions(headers, raw);
     const response = await sendDBRequest(serverSettings.url, requestOptions);
